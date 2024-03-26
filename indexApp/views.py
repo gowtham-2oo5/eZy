862c8f13b1c945ea4cd0e21775849e56d46fd7ad
Ala Gowtham Siva Kumar <111774754+gowtham-2oo5@users.noreply.github.com>
2024-03-26 12:06:03 +0530

    Update views.py

--- indexApp/views.py
+++ indexApp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from django.https import HttpResponse
-
 def index(request):
-    return HttpResponse(request,'Hello World')
-    #return render(request, 'home/index.html')
+    return render(request, 'home/index.html')
 def about(request):
     return render(request, 'home/about.html')
 def contact(request):
