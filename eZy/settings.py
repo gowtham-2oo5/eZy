5609190af32faa68b165236fb737b3ec403bedc1
gowtham-2oo5 <gowtham.ala12345@gmail.com>
2024-04-27 04:08:10 +0530

    Mhmm

--- eZy/settings.py
+++ eZy/settings.py
@@ -122,10 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'main/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "main/static",
-    "main/static/",
+    BASE_DIR / "/static",
 ]
 
 # Default primary key field type
